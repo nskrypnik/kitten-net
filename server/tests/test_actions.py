e5349d05222ef21f0248098e1e3598d7329a17fc
bogdan1988 <bogdan.marko@gmail.com>
2011-11-05 23:35:57 +0200

    test for auth

--- server/tests/test_actions.py
+++ server/tests/test_actions.py
@@ -15,6 +15,7 @@ class DumbSocket(object):
     
     def send(self, msg):
         print msg
+    
 
 dumb_socket = DumbSocket()
 connection = Connection(dumb_socket)
@@ -27,7 +28,12 @@ class CommandsTest(unittest.TestCase):
         user = User.load("bogdan@mail.ua")
         self.assertEqual(user.email, "bogdan@mail.ua")
 
-
+    def test_auth_not_user(self):
+	print "Yo"
+	actions.register(connection, email='bogdan1@mail.ua', password='bogdan19861')
+	actions.auth(connection, "bogdan@mail.ua", password='bogdan19861', token=None)
+	params = connection.authenticate(user)
+	self.assertEqual(params, 'Fail')
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()	
