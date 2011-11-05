1fc5b46fda0f0e79eeb98733ade82a4f8e34e9a2
Niko Skrypnik <nskrypnik@gmail.com>
2011-11-05 21:05:51 +0200

    Unstable auth

--- server/commands.py
+++ server/commands.py
@@ -1,7 +1,10 @@
 #Here is the commands of our system
 
 import sys
+import hashlib
+
 from usres import User
+
 this_module = sys.modules[__name__]
 
 def handle_request(json_req, connection):
@@ -27,4 +30,28 @@ def register(connection, **params):
     params = {'result': 'Ok', 'token': user.token}
     response_msg = {'cmd': 'register', 'params': params}
     connection.response(response_msg)
+
+def auth(connection, email, password=None, token=None):
+    user = User.load(email)
+    if not user:
+        params = {'result': 'Fail', 'errormsg': 'No such user'}
+        connection.response({'cmd': 'auth', 'params': params})
+        return
+    auth_succesful = False
+    if token and token == user.token:
+            auth_succesful = True
+    if password and hashlib.md5(password).hexdigest() == user.password:
+        auth_succesful = True
     
+    if auth_succesful:
+        user.update_token()
+        params = {'result': 'Ok', 'token': user.token}
+        connection.response({'cmd': 'auth', 'params': params})
+    else:
+        params = {'result': 'Fail', 'errormsg': 'Authentication failed'}
+        connection.response({'cmd': 'auth', 'params': params})
+
+
+    
+
+
